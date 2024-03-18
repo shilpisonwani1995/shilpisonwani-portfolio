e7005cd2328d4b196468c3d7536716d0b2d46f04
shilpisonwani1995 <shilpisonwani1995@gmail.com>
2024-03-18 23:31:14 +0530

    css change

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -70,7 +70,7 @@ const Experience = () => {
   ];
 
   return (
-    <div name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+    <div name='experience' className='bg-gradient-to-b from-gray-800 to-black w-full hscreen'>
 
       <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
         
